d255dc161a5d3007399f4711853d8a5c3279d519
b10t <b10t.rus@gmail.com>
2022-01-31 05:32:11 +0000

    Читаем переменные окружения.

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 
 class Settings():
-    EVERNOTE_CONSUMER_KEY = ''
-    EVERNOTE_CONSUMER_SECRET = ''
-    EVERNOTE_PERSONAL_TOKEN = ''
+    EVERNOTE_CONSUMER_KEY = os.getenv('EVERNOTE_CONSUMER_KEY', '')
+    EVERNOTE_CONSUMER_SECRET = os.getenv('EVERNOTE_CONSUMER_SECRET', '')
+    EVERNOTE_PERSONAL_TOKEN = os.getenv('EVERNOTE_PERSONAL_TOKEN', '')
 
-    JOURNAL_TEMPLATE_NOTE_GUID = ''
-    JOURNAL_NOTEBOOK_GUID = ''
+    JOURNAL_TEMPLATE_NOTE_GUID = os.getenv('JOURNAL_TEMPLATE_NOTE_GUID', '')
+    JOURNAL_NOTEBOOK_GUID = os.getenv('JOURNAL_NOTEBOOK_GUID', '')
 
-    INBOX_NOTEBOOK_GUID = ''
+    INBOX_NOTEBOOK_GUID = os.getenv('INBOX_NOTEBOOK_GUID', '')
+    SANDBOX = os.getenv('SANDBOX', False)
